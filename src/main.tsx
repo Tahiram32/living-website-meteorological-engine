2f774e14f04956b830673b1ea210aaf692c8e2f2
Tahira Muhammad <tahira.muhammad@student.csn.edu>
2026-07-21 17:39:18 -0700

    fix: set DemoDashboard as the root route for Vercel preview

--- src/main.tsx
+++ src/main.tsx
@@ -23,8 +23,8 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Storefront />} />
-        <Route path="/demo" element={<DemoDashboard />} />
+        <Route path="/" element={<DemoDashboard />} />
+        <Route path="/store" element={<Storefront />} />
         <Route path="/internal-fleet-admin" element={<AdminDashboard />} />
         <Route path="/cancel-articles/:domain" element={<CancelArticles />} />
       </Routes>
